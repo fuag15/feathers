3f8ed57042394d8ca105d9238f34b3fb43299ab0
Michael Boone <fuag155555@gmail.com>
2013-05-14 20:22:38 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feathers::Application.config.secret_token = '0f44e390aa1f19f1caac34e5df0998190329f41fc47241e10c50c2a9bd83b5bc403559884729d64b69bdebefc0cbad446985c1caa4412647ca381d82adff826c'
